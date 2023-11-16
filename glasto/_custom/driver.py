cee59db055c0423a3e5001a391b5f9046dcd39af
Jack Boreham <jack.boreham@bbc.co.uk>
2023-11-16 18:38:44 +0000

    Initial commit

---
+++ glasto/_custom/driver.py
@@ -0,0 +1,1 @@
+DRIVER_PATH='/Users/borehj01/code/mess-around/chromedriver-mac-x64/chromedriver'
---
+++ test.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+# driver initialization
+driver = webdriver.Chrome()
+# launch URL
+driver.get("https://www.tutorialspoint.com/index.htm")
